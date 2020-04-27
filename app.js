d0c7434e456edde5ffb6f81a0256847ff02d7cf1
olanetsoft <heedris2olubisi@gmail.com>
2020-04-27 15:49:58 +0100

    CREATED GET REQUEST ENDPOINT

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 
-const port = process.env.PORT
-app.listen(port, ()=>{
-    console.log(`Bro i dey listen to ${port}`);
+
+//Reading the file from a data
+const theTours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res)=>{
+    res.status(200).json({
+        status: 'success',
+        result: theTours.length,
+        data: {
+            theTours
+        }
+    })
+}); 
+
+
+
+
+
+app.listen('3000', ()=>{
+    console.log('Bro i dey listen to 3000');
 });
