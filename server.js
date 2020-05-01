a763754be4cb0399cb296abf0d40e4004b173501
olanetsoft <heedris2olubisi@gmail.com>
2020-05-01 20:36:33 +0100

    ESLINT INSTALLED FOR DEVELOPMENT

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ dotenv.config({path: './config.env'})
 
 // console.log(process.env);
 
-app.listen('3000', () => {
-    console.log('App running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`App running on port ${process.env.PORT}`);
 });
