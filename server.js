8ce6ae3c1bdd92f816af0593d1729e16bb2422d9
olanetsoft <heedris2olubisi@gmail.com>
2020-05-02 21:54:45 +0100

    DB CONNECTED SUCCESSFULLY

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 
 //using the dotenv variable
-dotenv.config({path: './config.env'})
+dotenv.config({ path: './config.env' });
+
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(result => {
+    console.log('DB Connected successfully')
+}).catch(err => {
+    console.log(err)
+});
+
+
 
 // console.log(process.env);
-
-app.listen(process.env.PORT, () => {
-    console.log(`App running on port ${process.env.PORT}`);
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
 });
