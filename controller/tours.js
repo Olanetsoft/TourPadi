1109359fcaca9cc5a72618a1bbce054282e4a8ed
olanetsoft <heedris2olubisi@gmail.com>
2020-05-13 21:24:39 +0100

    PAGINATION COMPLETED

--- controller/tours.js
+++ controller/tours.js
@@ -71,6 +71,19 @@ exports.getTours = async (req, res, next) => {
         }
 
 
+        //4) Pagination
+        //get the page
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page){
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error("This page doesn't exist")
+        }
+
+
 
         //EXECUTE THE QUERY_OBJ
         const allTours = await query;
