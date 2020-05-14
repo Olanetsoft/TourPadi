c94d8eb6f67d1f0997a63ba851215d116e1a4f9f
olanetsoft <heedris2olubisi@gmail.com>
2020-05-14 23:59:39 +0100

    CREATED GLOBAL ERROR HANDLER

--- app.js
+++ app.js
@@ -28,11 +28,29 @@ app.use(tourRoutes);
 
 //Implement a handler to handle all non-existing route
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'Failed !',
-        message: `Sorry can't find ${req.originalUrl} on the server😫😫`
-    })
+    // res.status(404).json({
+    //     status: 'Failed !',
+    //     message: `Sorry can't find ${req.originalUrl} on the server😫😫`
+    // });
+    const err = new Error(`Sorry can't find ${req.originalUrl} on the server😫😫`);
+    err.status = 'fail';
+    err.statusCode = 400;
+
+    next(err)
 });
 
+
+//error handling middleware
+app.use((err, req, res, next) => {
+    //gets the statusCode
+    err.statusCode = err.statusCode || 500;
+    //gets the status
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+});
 
 module.exports = app;
