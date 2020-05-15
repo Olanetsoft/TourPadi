a4d7d37bdb7b29c653f2e78a19f21f27e011fa9c
olanetsoft <heedris2olubisi@gmail.com>
2020-05-15 15:24:09 +0100

    ERROR ON DEVELOPMENT AND PRODUCTION COMPLETED

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,11 +1,48 @@
+//Send error for development env
+const sendDevError = (err, res)=> {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+//send error for production env
+const sendProdError = (err, res) => {
+    //Operational, trusted error: send message to client
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    }
+    //programming error, Don't leak error details
+    else{
+        //1) Log error
+        console.error('ERROR ☹', err);
+
+        //2) Send generic error
+        res.status(500).json({
+            status: 'error 🙄',
+            message: 'Something went wrong !'
+        });
+    };
+    
+};
+
+
+//export moodule to handle global error
 module.exports = (err, req, res, next) => {
     //gets the statusCode
     err.statusCode = err.statusCode || 500;
     //gets the status
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
+    if(process.env.NODE_ENV == 'development'){
+        sendDevError(err, res);
+    }
+    else if(process.env.NODE_ENV == 'production'){
+      sendProdError(err, res);
+    };
 };
