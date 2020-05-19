38e14c297aa80dc856ae719bab36a4691bd3ede8
olanetsoft <heedris2olubisi@gmail.com>
2020-05-19 15:50:44 +0100

    IMPLEMENTED DATA SANITATION

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 
-//importing helmet to set a secure headers
+//Requiring mono sanitizer
+const mongoSanitizer = require('express-mongo-sanitize');
+
+//Requiring mono sanitizer
+const xss = require('xss-clean');
+
+//Requiring helmet to set a secure headers
 const helmet = require('helmet');
 
-//importing express rate limit
+//Requiring express rate limit
 const rateLimit = require('express-rate-limit');
 
 //import global error class
@@ -43,9 +49,10 @@ app.use(express.json({ limit: '10kb' }));
 
 
 //Data sanitization against NoSql query injection
-
+app.use(mongoSanitizer());
 
 //Data sanitization against XSS
+app.use(xss());
 
 
 //registering a middleware for server static files
