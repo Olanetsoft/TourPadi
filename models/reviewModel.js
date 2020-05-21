3837edcc84a6d3a323d4cd651a9e2070ac6be16d
olanetsoft <heedris2olubisi@gmail.com>
2020-05-21 14:04:03 +0100

    REVIEWS MODEL CREATED

---
+++ models/reviewModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    review: {
+        type: String,
+        required: [true, 'Review can not be empty']
+
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    tour: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour', tourSchema,
+        required: [true, 'Review must belong to a tour.']
+
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Review must belong to a user 😦 ']
+    }
+},
+    {
+        timestamps: true,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+//to make the virtual show up when a request is made you need to enable it here in the schema
+// {
+//     toJSON: { virtuals: true },
+//     toObject: { virtuals: true }
+// });
+
+//define the Review Model
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
